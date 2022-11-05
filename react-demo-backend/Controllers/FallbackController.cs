b8c82a29fe9447fc44e28abab86aa5004ead4fb4
Maxim Schekolda <maxsandi.work@gmail.com>
2022-11-05 13:37:47 +0200

    1

---
+++ react-demo-backend/Controllers/FallbackController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Net.Mime;
+
+namespace react_demo_backend.Controllers
+{
+    public class FallbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
+                "wwwroot", "index.html"), MediaTypeNames.Text.Html);
+        }
+    }
+}
